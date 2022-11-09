a71ce1825b27d816fb4e88d2a66bd3f6d9239997
Andy <andyliao292@gmail.com>
2022-11-09 20:46:51 -0300

    Add file, 1ºcommit

---
+++ GeradorDeSenha.py
@@ -0,0 +1,23 @@
+import secrets, string
+
+print("-"*5 + "GERADOR DE SENHA" + "-"*5)
+letra = string.ascii_letters
+numero = string.digits
+simbolo = "!@#$%&*_"
+tamanho = input("Escolha o tamanho da senha: ")
+
+senha_caracter = ''.join(secrets.choice(simbolo)) + ''.join(secrets.choice(letra) for i in range(int(tamanho) - 1))
+senha_numero = ''.join(secrets.choice(numero) for i in range(int(tamanho)))
+
+opcao = input("Deseja gerar senha ou pin ?").lower().strip()
+
+while True:
+    if opcao == "pin":
+        print(f"O pin gerado é {senha_numero}")
+        break
+    if opcao == "senha":
+        print(f"A senha gerada é `{senha_caracter}")
+        break
+    if opcao != "pin" or opcao != "senha":
+        print("Digite uma opção válida!")
+        opcao = input("Deseja gerar senha ou pin ?").lower().strip()
